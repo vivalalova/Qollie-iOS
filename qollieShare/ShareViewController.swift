dd60c0ab2ea0cf43d078e2033222c91214aa6122
Lova <vivalalova0@gmail.com>
2017-02-27 05:27:10 +0800

    update

--- qollieShare/ShareViewController.swift
+++ qollieShare/ShareViewController.swift
@@ -14,26 +14,12 @@ class LOViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         guard let item = extensionContext?.inputItems.first as? NSExtensionItem, let itemProvider = item.attachments?.first as? NSItemProvider else {
-            self.alert(message: "哎呀? 意外地不能使用")
+            self.alert(message: "哎呀? 請在104 APP內使用喔")
             return
         }
-
-//        if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-//            itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, err) in
-//                guard let url = url as? URL else {
-//                    self.alert(message: "我們不支援從瀏覽器開啟喔,請使用104 APP")
-//                    return
-//                }
-//                //url.host
-//                //https://m.104.com.tw/job/5b00o?jobsource=m104_hotorder
-//
-//                self.handle(url: url)
-//
-//            })
-//        } else
-
+        
         if itemProvider.hasItemConformingToTypeIdentifier("public.plain-text") {
             itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: { (text, err) in
                 guard let text = text as? String else {
@@ -43,29 +29,23 @@ class LOViewController: UIViewController {
 
                 self.handle(text: text)
             })
+        } else {
+            self.alert(message: "哎呀? 請在104 APP內使用喔")
         }
     }
 
-    func handle(url: URL) {
-        print(url.absoluteString)
-
-    }
-
     func handle(text: String) {
-        print(text)
-
         let array = text.components(separatedBy: "-")
 
         guard let company = array.first?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
             let url = URL(string: "https://www.qollie.com/search?keyword=\(company)&kind=company") else {
-                self.alert(message: "")
+                self.alert(message: "請在104 APP內使用喔")
                 return
         }
 
-
-        print(url.absoluteString)
         let safari = SFSafariViewController(url: url)
         safari.delegate = self
+
         self.present(safari, animated: true, completion: nil)
     }
 
@@ -75,13 +55,14 @@ class LOViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "確定", style: .cancel) { action in
             self.dismiss()
         })
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
     func dismiss() {
         self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
     }
 }
-
 
 extension LOViewController: SFSafariViewControllerDelegate {
     func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
