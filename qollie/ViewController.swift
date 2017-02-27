34caf2a649ae87cb695a41141129629b2bb0aac1
Lova <vivalalova0@gmail.com>
2017-02-27 19:42:26 +0800

    打開104

--- qollie/ViewController.swift
+++ qollie/ViewController.swift
@@ -16,18 +16,47 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
-        
+
         Answers.logCustomEvent(withName: "打開APP耶", customAttributes: nil)
-        
+
         self.setNeedsStatusBarAppearanceUpdate()
     }
 
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(scrollView.contentOffset.x / self.view.frame.size.width)
     }
-    
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 }
 
+
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var openUrlBtn: UIButton?
+
+    let e04 = "fb215646968788192://"
+    var url: URL? {
+        return URL(string: self.e04)
+    }
+
+    var canOpen104: Bool {
+        if let url = url {
+            return UIApplication.shared.canOpenURL(url)
+        }
+        return false
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        openUrlBtn?.isHidden = !self.canOpen104
+    }
+
+    @IBAction func go104(_ sender: UIButton) {
+        if canOpen104, let url = url {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
